3fc820fae683eddf26f7fdbc0f8bc9dffe033222
hobambi <yyunsunny@gmail.com>
2023-02-02 22:17:44 +0900

    sosu

--- Solution.java
+++ Solution.java
@@ -1,11 +1,14 @@
-public class Solution {
-    int answer = 0;
-    int[] ch = new int[n+1];
-for (int i=2; i<=n; i++){
-        if (ch[i]==0){
-            answer++;
-            for (int j=i; j<n+1; j=j+i) ch[j] = 1;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] ch = new int[n+1];
+        for (int i=2; i<=n; i++){
+            if (ch[i]==0){
+                answer++;
+                for (int j=i; j<n+1; j=j+i) ch[j] = 1;
+            }
         }
+        return answer;
     }
-return answer;
 }
