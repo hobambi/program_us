a41266e15eddcdf946304347eaaa61d95efe41bd
hobambi <yyunsunny@gmail.com>
2023-02-02 21:46:06 +0900

    git test2

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 public class Main {
-    public int solution(String s) {
-        int answer = 0; // 최종 점수
-        int count = 1; // 현재 점수를 세는 변수
-        for (int i = 0; i < s.length(); i++) { // OX를 하나씩 다 받음
-            if (s.charAt(i) == 'O') { // O면 정답 맞은거니까
-                answer += count; // 최종 점수에 현재 점수 넣어줌
-                count += 1; // 연속으로 나오면 점수가 늘어나니까 점수 증가
-            } else { // O가 아닐때 X일때니까
-                count = 1; // 증가하던 점수 초기화
-            }
+    public int solution(int num) {
+        int answer = 0;
+        int[] money = new int[]{500, 100, 50, 10};
+        int index = 0;
+        num = 1000 - num;
+        while (num > 0) {
+            answer += num/money[index];
+            num -= (num/money[index])*money[index];
+            index +=1;
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Main method = new Main();
-        String s = "OXOOOXXXOXOOXOOOOOXO";
-        System.out.println(method.solution(s));
+        int num1 = 160;
+        System.out.println(method.solution(num1));
     }
 }
